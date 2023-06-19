af924312d254366888fdcf2fcd421e8a08ee1f89
ultra_kevyk <strejdaniel@gmail.com>
2023-06-19 20:02:09 +0200

    first commit

---
+++ back.py
@@ -0,0 +1,75 @@
+import requests
+import csv
+import pickle
+import os
+
+
+data_dir = r"C:\Python\Fishing_calculator\Lib\site-packages\fishing\\"
+size_dict = {}
+name_dict = {}
+
+def average_weight(area, fish):     # calculate average weight of specific fish on specific area
+    with open(data_dir + area + ".csv", "r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if fish in row:
+                try:
+                    average_weight = float((row)[2]) / float((row)[1])
+                except Exception:
+                    break
+                return average_weight
+
+
+def fish_ratio(area, fish, area_size):  # calculate fish/ha ratio on specific area
+    with open(data_dir + area + ".csv", "r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if fish in row:
+                try:
+                    ratio = float((row)[1]) / area_size
+                except Exception:
+                    print()
+                    break
+                return ratio
+
+
+def global_average_weight(directory, fish):     # Getting average weight for specific fish on all areas
+    total_weight = 0
+    num_weights = 0
+    weight = 0
+    for file in os.listdir(directory):
+        if file.endswith(".csv"):
+            with open(os.path.join(directory, file), "r", encoding="utf-8") as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    if fish in row:
+                        try:
+                            total_weight += (float(row[2]) / float(row[1]))
+                            num_weights += 1
+                        except Exception:
+                            continue
+        try:
+            weight = total_weight / num_weights
+        except Exception:
+            continue
+    return  weight
+
+
+def global_average_ratio(directory, fish, area_sizes):
+    total_ratio = 0
+    num_ratios = 0
+    for file in os.listdir(directory):
+        if file.endswith(".csv"):
+            with open(os.path.join(directory, file), "r", encoding="utf-8") as file:
+                file.name
+                name = os.path.basename(file.name).strip(".csv")
+                reader = csv.reader(file)
+                for row in reader:
+                    if fish in row:
+                        try:
+                            total_ratio += float(row[1]) / area_sizes[name]
+                            num_ratios += 1
+                        except Exception:
+                            continue
+
+    return  total_ratio / num_ratios
---
+++ website/views.py
@@ -0,0 +1,73 @@
+from flask import Blueprint, render_template, flash, request
+import requests
+import csv
+import pickle
+import os
+from back import fish_ratio, average_weight, global_average_weight, global_average_ratio
+
+data_dir = r"C:\Python\fishing_calc\VE\website\data\downloaded_data\\"
+size_dict = {}
+name_dict = {}
+
+with open(data_dir + "SIZES.pkl", 'rb') as f:
+    size_dict = pickle.load(f)
+with open(data_dir + "NAMES.pkl", 'rb') as g:
+    name_dict = pickle.load(g)
+with open(data_dir + "FISH.pkl", 'rb') as h:
+    fish_list = pickle.load(h)
+
+avrg_weights = {}
+for fish in fish_list:
+    avrg_weights[fish] = global_average_weight(data_dir, fish)
+avrg_ratios = {}
+for fish in fish_list:
+    avrg_ratios[fish] = global_average_ratio(data_dir, fish, size_dict)
+
+
+views = Blueprint('views', __name__)
+
+@views.route('/', methods=['POST', 'GET'])
+def home():
+    return render_template("home.html")
+
+@views.route('/', methods=['POST', 'GET'])
+def output():
+    return render_template("output.html")
+
+@views.route('/calc', methods=['GET', 'POST'])
+def calc():
+    if request.method == 'POST':
+        inputed_area = request.form.get('areas')
+        inputed_fish = request.form.get('fish')
+        for key, value in name_dict.items():
+            if key == inputed_area:
+                name = value
+                number = key
+                size = size_dict[key]
+
+                o_weight = ""
+                if average_weight(key, inputed_fish):
+                    weight = round(average_weight(key, inputed_fish), 2)
+                    if average_weight(key, inputed_fish) > avrg_weights[inputed_fish]:
+                        o_weight = "více"
+                    else:
+                        o_weight = "méně"
+                    avrg_w = round(avrg_weights[inputed_fish], 3)
+
+                o_ratio = ""
+                if fish_ratio(key, inputed_fish, size_dict[key]):
+                    ratio = round(fish_ratio(key, inputed_fish, size_dict[key]), 2)
+                    if float(fish_ratio(key, inputed_fish, size_dict[key]) > float(avrg_ratios[inputed_fish])):
+                        o_ratio = "více"
+                    else:
+                        o_ratio = "méně"
+                    avrg_r = round(avrg_ratios[inputed_fish], 2)
+                    return render_template('output.html', inputed_fish=inputed_fish, size=size, name=name, number=number,
+                                           weight=weight, ratio=ratio, o_weight=o_weight, o_ratio=o_ratio, avrg_w=avrg_w,
+                                           avrg_r=avrg_r)
+                else:
+                    flash(f"No {inputed_fish} caught on {value} yet!", category='success')
+
+
+                
+    return render_template("calc.html")
